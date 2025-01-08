69f3d512580572f561d4c4e850bee88cc28258cc
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2025-01-08 03:18:16 -0500

    Añadiendo el sidebar

---
+++ resources/js/Layouts/PortafolioLayout.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Inicio',
+});
+</script>
+
+<template>
+    <div>
+        Este sera el Sidebar del Portafolio
+    </div>
+</template>
